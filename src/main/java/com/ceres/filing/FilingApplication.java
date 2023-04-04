8333385a21aace6163bb6bd14add316b2944d96f
xandu1738 <xandu1738@gmail.com>
2023-04-04 11:20:39 +0300

    ...

---
+++ src/main/java/com/ceres/filing/FilingApplication.java
@@ -0,0 +1,13 @@
+package com.ceres.filing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilingApplication.class, args);
+    }
+
+}
